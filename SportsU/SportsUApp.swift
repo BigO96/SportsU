04ab27c16d9d8bfd4553819af988f0438d2f0ef0
Oscar Epp <oepp@uoregon.edu>
2024-05-13 17:49:30 -0700

    Initial Commit

---
+++ SportsU/SportsUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsUApp.swift
+//  SportsU
+//
+//  Created by Oscar Epp on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SportsUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
